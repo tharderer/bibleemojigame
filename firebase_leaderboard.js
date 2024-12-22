28bdbea32c7c893d1d0377e057a52c26a60b20de
Tim Harder <tharderer@users.noreply.github.com>
2024-12-21 22:35:34 -0600

    Update firebase_leaderboard.js

--- firebase_leaderboard.js
+++ firebase_leaderboard.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
 /**
  * Save player's time to the leaderboard.
  * @param {string} playerName - Name of the player.
